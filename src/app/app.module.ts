7ad312f2c64acb4ddadecf086fdefa636736a3ef
sauravphulera <phulerasanju20@gmail.com>
2020-08-26 00:13:25 +0530

    added starting animation and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupContainerComponent } from './signup-container/signup-container.component';
 import { SigninContainerComponent } from './signin-container/signin-container.component';
-import { OverlayContainerComponent } from './overlay-container/overlay-container.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent,
     SignupContainerComponent,
     SigninContainerComponent,
-    OverlayContainerComponent
   ],
   imports: [
     BrowserModule,
