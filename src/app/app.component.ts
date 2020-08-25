a472070937ac6e4a200f5436126db3c362fde533
sauravphulera <phulerasanju20@gmail.com>
2020-08-25 23:10:27 +0530

    overlay and signin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component, ElementRef, ViewChild, Renderer2 } from "@angular/core";
+import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild("container") container: ElementRef;
+  @ViewChild('container') container: ElementRef;
 
-  title = "old-frog-animapp";
+  title = 'old-frog-animapp';
   isSignUpClicked = false;
 
   constructor(private renderer: Renderer2) {}
 
   onSignInClick() {
-    this.isSignUpClicked = true;
-    this.renderer.removeClass(this.container.nativeElement, "right-panel-active");
+    this.isSignUpClicked = false;
+
+    this.renderer.removeClass(this.container.nativeElement, 'right-panel-active');
   }
 
   onSignUpClick() {
-    this.renderer.addClass(this.container.nativeElement, "right-panel-active");
+    this.isSignUpClicked = true;
+
+    this.renderer.addClass(this.container.nativeElement, 'right-panel-active');
   }
 }
